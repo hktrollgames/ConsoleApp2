aa361fbf483f3dbaf3df34f16128760db441cd1e
T1 <hktrollgames@gmail.com>
2022-05-15 17:18:46 +0900

    FindNumber

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,25 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("마지막");
+            //배열에서 3보다 큰 짝수를 찾는 함수 만들기
+            int[] testArray = new int[] { 1, 56, 2, 3, 4, 6 };
+            int returnNumber = FindNumber(testArray);
+            Console.WriteLine(returnNumber);
+        }
+
+        private static int FindNumber(int[] testArray)
+        {
+            for (int i = 0; i < testArray.Length; i++)
+            {
+                int number = testArray[i];
+                int remain = number % 2;
+                if (remain == 0)
+                {
+                    return number;
+                }
+            }
+
+            return -1;
         }
     }
 }
