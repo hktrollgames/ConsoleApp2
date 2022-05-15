f18a09a65b54ef0a93ca5b6a90c72099c754ccd1
T1 <hktrollgames@gmail.com>
2022-05-15 17:51:19 +0900

    And, Or조건 설명

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,12 +10,24 @@ namespace ConsoleApp2
         {
             // 1. 배열에서 3보다 큰 짝수를 찾는 함수 만들기
             // 2. 가장 작은 짝수를 리턴하자
-            List<int> testArray = new List<int>{ 1, 56, 2, 3, 4, 6 };
-            int returnNumber = testArray.Where(x => x > 3 && x % 2 == 0)
+            List<int>   testList    = new List<int> { 1, 56, 2, 3, 4, 6 };
+            int[]       testArray   = new int[]     { 1, 56, 2, 3, 4, 6 };
+            int returnNumber = testList.Where(x => x > 3 && x % 2 == 0)
                 .OrderBy(x => x)
                 .First();
 
             Console.WriteLine(returnNumber);
+
+            //&& -> And 조건 : true true일때 참
+            // a && b : a, b랑 둘다 참일때 참
+            bool a = true;
+            bool b = true;
+            bool c = a && b;// true or false; -> true
+            b = false;
+            c = a && b;     // c -> false
+
+            //||// or : 둘중 하나라도 참이면 true;
+            c = a || b;     // c : true;
         }
     }
 }
